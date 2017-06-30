d53fa4184ddbd3f751c859bfe6f269c72beaec81
gravesjake <jacob.graves@ucdenver.edu>
2017-06-30 17:49:26 -0600

    Improved mouseover performance of RF Spectrum subwindow

--- shinysdr/i/webstatic/client/widgets/spectrum.js
+++ shinysdr/i/webstatic/client/widgets/spectrum.js
@@ -1164,22 +1164,60 @@ define(['./basic', './dbui',
     function addBand(record) {
       const el = document.createElement('span');
       el.className = 'freqscale-band';
+
+      // JG add start
       /* 
         JG this is where the label text is set, could possibly use this method and make our own
         text drawing function and pull in the record.label || record.mode content to our draw
         or possibly just modify the existing draw to do what we want
+
+        Add a button in the menu for the user to select or deselect the new label method
       */
+      // this draws a box on bottom, not what we need
+      var rfWindow = document.getElementById("rf-spectrum-monitor");
+      var textBox = rfWindow.appendChild(document.createElement('div'));
+      textBox.textContent = record.label || record.mode;  // this looks really bad, prints a bunch of times, maybe because it's a div?
+      console.log("addBand");
+      // JG add end
+
       el.textContent = record.label || record.mode;
-      //el.textContent = "textContent";
       el.my_update = function () {
-        console.log("\n\naddBand:el.my_update = function()\n\n");   // JG added this 
+        console.log("\n\naddBand:el.my_update = function()\n\n");   // JG added
         var labelLower = Math.max(record.lowerFreq, lower);
         var labelUpper = Math.min(record.upperFreq, upper);
         el.style.left = view.freqToCSSLeft(labelLower);
         el.style.width = view.freqToCSSLength(labelUpper - labelLower);
         el.style.bottom = pickY(record.lowerFreq, record.upperFreq) + 'em';
-        // JG this style stuff just says where to draw the band labels on the window
-        // JG they all get drawn on top of each other when they're gone, but still drawn
+
+        // JG add start
+        var mouse_x_pos;
+        var left;
+        var width;
+
+        var offset = $("#rf-spectrum-monitor").offset();
+        $(document).mousemove(function(e) {
+          //console.log("mousemove function");
+          mouse_x_pos = e.pageX - offset.left;
+          //console.log("mouse_x_pos: ", mouse_x_pos);
+          //console.log("el.style.left: ", el.style.left);
+          //console.log("el.style.width: ", el.style.width);
+          left = parseFloat(el.style.left);
+          width = parseFloat(el.style.width);
+          //console.log("left: ", left);
+          //console.log("width: ", width);
+
+          // this draws the correct label on mouseover but the mouse has to leave the RF Spectrum 
+          // window to change which label gets drawn
+          if (mouse_x_pos > left && mouse_x_pos < left + width) {
+            //console.log("if success");
+            el.style.display = 'block';
+          }
+          else {
+            //console.log("else success");
+            el.style.display = 'none';
+          }
+        })
+        // JG add end
       };
       return el;
     }
@@ -1261,9 +1299,11 @@ define(['./basic', './dbui',
       startup as in when the user mouses over any of the window
       should find a better way to do this as it doesn't guarantee that the labels won't be drawn on startup
       and I still need to change labels to only draw one label and draw it where the mouse is on the spectrum
+
+      Add a menu button to select/deselect this label method
     */
 
-    // initialize hidden labels
+    // initialize hidden labels, this can go away once we have the menu button
     if (!drawFlag) {
       document.addEventListener("mouseover", menuFunc);
       function menuFunc() {
@@ -1278,7 +1318,7 @@ define(['./basic', './dbui',
         console.log("drawFlag: ", drawFlag);
       }
     }
-
+/*
     // draw labels on RF Spectrum window mouseover
     document.getElementById("rf-spectrum-monitor").addEventListener("mouseover", drawFunc);
     function drawFunc() {
@@ -1286,10 +1326,13 @@ define(['./basic', './dbui',
       //draw();
       var labels = document.getElementsByClassName("freqscale-band");
         for (var i = 0; i < labels.length; ++i) {
+          // maybe try adding something here to check mouse location on the window
+          // and only draw a label if the mouse is within the width of that label
+          // so there can be at most one label drawn at a time
           document.getElementsByClassName("freqscale-band")[i].style.display = 'block';
         }
     }
-
+   
     // hide labels on RF Spectrum window mouseout
     document.getElementById("rf-spectrum-monitor").addEventListener("mouseout", hideFunc);
     function hideFunc() {
@@ -1299,6 +1342,7 @@ define(['./basic', './dbui',
           document.getElementsByClassName("freqscale-band")[i].style.display = 'none';
         }
     }
+*/
     /*
       This successfully draws the original annotation method on mouseover of the RF Spectrum window
       but it persists when mouse is no longer over the RF window
